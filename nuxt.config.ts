d923ea4ad5888519489dddca2fb8434454a39ef1
Miqayel <miqayelpetrosyan@gmail.com>
2026-05-12 00:58:37 +0400

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,9 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { hid: 'description', name: 'description', content: 'Developer Portfolio of Miqayel Petrosyan' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg', href: '/favicon.svg' }
       ]
     }
   }
